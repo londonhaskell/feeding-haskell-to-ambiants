fcc982888bcde5f7c765baa11f2b6554dbf4da84
Stuart Gale <bishboria@gmail.com>
2013-11-19 19:57:28 +0000

    Create World session.

--- puzzles/1.World.hs
+++ puzzles/1.World.hs
@@ -1,37 +1,56 @@
 module World where
 
--- Create the Pos and Dir data types (Section 2.1, p3 lines 2 and 5)
+import           Prelude hiding (id)
+import qualified Data.Map as M
 
--- Pos type ...
-data Pos = ToDoPos  -- You might want to change this from a data declaration
+data Pos = Pos Int Int
+           deriving (Show, Eq, Ord)
 
--- Dir type ...
-data Dir = ToDoDir
+data Dir = East
+         | SouthEast
+         | SouthWest
+         | West
+         | NorthWest
+         | NortEast
+         deriving (Enum, Show, Eq, Ord, Read)
 
-
--- Create the Color data type and other_color function (line 2 and 4 of Section 2.2, p4)
-
-data Color = ToDoColour
+data Color = Red
+           | Black
+           deriving (Show, Eq, Read)
 
 other_color :: Color -> Color
-other_color = undefined
+other_color Red   = Black
+other_color Black = Red
+
+type BrainState = Integer
+mkBrainState :: Integer -> BrainState
+mkBrainState i
+  | i >= 0 && i <= 9999 = i
+mkBrainState n          = 0
 
 -- Create the Ant data type (line 8 of section 2.2, p4) and the 
 -- accessor and update functions (line 3 of p5, section 2.2)
 
-data Ant = ToDoAnt
+data Ant = Ant { id :: Integer
+               , color :: Color
+               , brainState :: BrainState
+               , resting :: Integer
+               , dir :: Dir
+               , hasFood :: Bool
+               }
+         deriving (Show, Eq, Read)
 
 set_state :: Ant -> Integer -> Ant
-set_state = undefined
+set_state a i = a { brainState = mkBrainState i }
 
 set_resting :: Ant -> Integer -> Ant
-set_resting = undefined
+set_resting a i = a { resting = i }
 
 set_direction :: Ant -> Dir -> Ant
-set_direction = undefined
+set_direction a d = a { dir = d }
 
 set_has_food :: Ant -> Bool -> Ant
-set_has_food = undefined
+set_has_food a b = a { hasFood = b }
 
 
 -- Define a Cell data structure that records all the 
@@ -48,30 +67,58 @@ set_has_food = undefined
 
 -- World, Cell, Marker type...
 
-data World = ToDoWorld
+type Marker = Integer
+mkMarker :: Integer -> BrainState
+mkMarker i
+  | i >= 0 && i <= 5 = i
+mkMarker n           = 0
 
--- Write rocky function (line 3 of section 2.3, p5)
+type FoodParticles = Integer
+mkFoodParticles :: Integer -> Integer
+mkFoodParticles i
+  | i >= 0        = i
+mkFoodParticles n = 0
+
+data Cell = Rocky
+          | Clear { ant           :: Maybe Ant
+                  , foodParticles :: FoodParticles
+                  , marker        :: Marker
+                  }
+          deriving (Show, Eq)
+
+data World = World (M.Map Pos Cell)
 
 rocky :: World -> Pos -> Bool
-rocky = undefined
+rocky (World w) p = case w M.! p of
+                    Rocky -> True
+                    _     -> False
 
 -- Define some_ant_is_at, ant_at and set_ant_at functions (line 14 onwards of 
 -- section 2.3, p5)
 
 some_ant_is_at :: World -> Pos -> Bool
-some_ant_is_at = undefined
+some_ant_is_at (World w) p = case w M.! p of
+                             Rocky             -> False
+                             Clear Nothing _ _ -> False
+                             Clear _       _ _ -> True
 
 ant_at :: World -> Pos -> Ant
-ant_at = undefined
+ant_at (World w) p = case w M.! p of
+    Clear (Just a) _ _ -> a
+    otherwise          -> error ("Invalid call to ant_at: <world map> at pos: " ++ show p)
 
 set_ant_at :: World -> Pos -> Ant -> World
-set_ant_at  = undefined
+set_ant_at x@(World w) p a = case w M.! p of
+                           Clear Nothing  f m -> World $ M.insert p (Clear (Just a) f m) w
+                           Clear (Just b) f m -> World $ M.insert p (Clear (Just a) f m) w
+                           Rocky              -> error ("Invalid: you can't set an ant on a Rocky cell")
+
                                                              
 
 -- Define ant_is_alive and find_ant functions (line 25 onwards of section 2.3, p5)
 
 ant_is_alive :: World -> Integer -> Bool
-ant_is_alive = undefined
+ant_is_alive (World w) antId = undefined
 
 find_ant :: World -> Integer -> Pos
 find_ant = undefined
