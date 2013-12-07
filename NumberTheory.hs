fa2697ae096e4b4461331e598c76344e1df8dbe2
Stuart Gale <bishboria@gmail.com>
2013-12-07 16:38:40 +0000

    Number Theory section of paper.

---
+++ NumberTheory.hs
@@ -0,0 +1,6 @@
+module RandomInt where
+
+randomInt :: Int -> [Int]
+randomInt = drop 4
+          . map ((`mod` 16384) . (`div` 65536))
+          . iterate ((`mod` (65536*16384)) . (1 +) . (22695477 *))
