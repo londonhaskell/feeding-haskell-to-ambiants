5e248db19f3bfcfb5c19f3ab6959e08aa4ca73e1
Stuart Gale <bishboria@gmail.com>
2013-12-21 14:02:22 +0000

    extract and use parseInt. Fix imports

--- NeuroCartography.hs
+++ NeuroCartography.hs
@@ -3,13 +3,12 @@ module NeuroCartography
       readBrainState
     ) where
 
-import qualified Data.Char                     as C        (digitToInt)
 import           Text.ParserCombinators.Parsec
 
 import           Biology
 import           Chemistry
 import           Geometry
-import qualified Geometry      as G
+import qualified Geometry as G
 import           Neurology
 import           Phenomenology
 
@@ -26,38 +25,35 @@ parseMark :: Parser Instruction
 parseMark = do
     string "Mark"
     char ' '
-    marker <- digit
+    marker <- parseInt
     char ' '
-    st <- many digit
-    return $ Mark (mkMarker $ C.digitToInt marker)
-                  (mkState (read st :: Int))
+    st <- parseInt
+    return $ Mark (mkMarker marker) (mkState st)
 
 parseUnmark :: Parser Instruction
 parseUnmark = do
     string "Unmark"
     char ' '
-    marker <- digit
+    marker <- parseInt
     char ' '
-    st <- many digit
-    return $ Unmark (mkMarker $ C.digitToInt marker)
-                    (mkState (read st :: Int))
+    st <- parseInt
+    return $ Unmark (mkMarker marker) (mkState st)
 
 parsePickUp :: Parser Instruction
 parsePickUp = do
     string "PickUp"
     char ' '
-    st1 <- many digit
+    st1 <- parseInt
     char ' '
-    st2 <- many digit
-    return $ PickUp (mkState (read st1 :: Int))
-                    (mkState (read st2 :: Int))
+    st2 <- parseInt
+    return $ PickUp (mkState st1) (mkState st2)
 
 parseDrop :: Parser Instruction
 parseDrop = do
     string "Drop"
     char ' '
-    st <- many digit
-    return $ Drop (mkState (read st :: Int))
+    st <- parseInt
+    return $ Drop (mkState st)
 
 parseTurn :: Parser Instruction
 parseTurn = do
@@ -65,32 +61,34 @@ parseTurn = do
     char ' '
     turn <- string "Left" <|> string "Right"
     char ' '
-    st <- many digit
-    return $ Turn (if turn !! 0 == 'L' then G.Left else G.Right)
-                  (mkState (read st :: Int))
+    st <- parseInt
+    return $ Turn (if turn !! 0 == 'L' then G.Left else G.Right) (mkState st)
 
 parseMove :: Parser Instruction
 parseMove = do
     string "Move"
     char ' '
-    st1 <- many digit
+    st1 <- parseInt
     char ' '
-    st2 <- many digit
-    return $ Move (mkState (read st1 :: Int))
-                  (mkState (read st2 :: Int))
+    st2 <- parseInt
+    return $ Move (mkState st1) (mkState st2)
 
 parseFlip :: Parser Instruction
 parseFlip = do
     string "Flip"
     char ' '
+    n <- parseInt
+    char ' '
+    st1 <- parseInt
+    char ' '
+    st2 <- parseInt
+    return $ Flip n (mkState st1) (mkState st2)
+
+parseInt :: Parser Int
+parseInt = do
     n <- many digit
-    char ' '
-    st1 <- many digit
-    char ' '
-    st2 <- many digit
-    return $ Flip (read n :: Int)
-                  (mkState (read st1 :: Int))
-                  (mkState (read st2 :: Int))
+    return (read n :: Int)
+
 
 readBrainState s = case parse parseInstruction "Brain State" s of
                      Prelude.Left  err -> "ERROR parsing: " ++ show err
