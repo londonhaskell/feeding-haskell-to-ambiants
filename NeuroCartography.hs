a5e627294b5fd0e5c6e9c4230e418c912cc5f47b
Stuart Gale <bishboria@gmail.com>
2013-12-22 07:52:11 +0000

    Parse Sense instruction.
    Need to change Marker condition to use real Marker type instead of Int; this will likely cause a few changes.

--- NeuroCartography.hs
+++ NeuroCartography.hs
@@ -20,6 +20,7 @@ parseInstruction = try (parseMark)     -- Move starts with 'M' so *try* Mark
                <|> parseTurn
                <|> parseMove
                <|> parseFlip
+               <|> parseSense
 
 parseMark :: Parser Instruction
 parseMark = do
@@ -89,6 +90,45 @@ parseInt = do
     n <- many digit
     return (read n :: Int)
 
+parseSenseDir :: Parser SenseDir
+parseSenseDir = do
+    dir <-     string "Here"
+           <|> string "Ahead"
+           <|> string "LeftAhead"
+           <|> string "RightAhead"
+    return (read dir :: SenseDir)
+
+parseCondition :: Parser Condition
+parseCondition = do
+    cond <-     try (string "FriendWithFood")
+            <|> try (string "Friend")
+            <|> try (string "FoeWithFood")
+            <|> try (string "FoeMarker")
+            <|> try (string "FoeHome")
+            <|> try (string "Foe")
+            <|> string "Food"
+            <|> string "Rock"
+            <|> string "Home"
+            <|> string "Marker"
+    case cond of
+      "Marker" -> do
+          char ' '
+          i <- parseInt
+          return $ Marker i -- not yet the chemical marker, so not using smart constructor
+      x        -> return (read x :: Condition)
+
+parseSense :: Parser Instruction
+parseSense = do
+    string "Sense"
+    char ' '
+    dir <- parseSenseDir
+    char ' '
+    st1 <- parseInt
+    char ' '
+    st2 <- parseInt
+    char ' '
+    cond <- parseCondition
+    return $ Sense dir (mkState st1) (mkState st2) cond
 
 readBrainState s = case parse parseInstruction "Brain State" s of
                      Prelude.Left  err -> "ERROR parsing: " ++ show err
