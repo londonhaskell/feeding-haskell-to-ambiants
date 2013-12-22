a59cfafec50de948977f68df02e4d850ea70b606
Stuart Gale <bishboria@gmail.com>
2013-12-22 08:37:45 +0000

    Extract and use parseMarker and parseState functions

--- NeuroCartography.hs
+++ NeuroCartography.hs
@@ -26,35 +26,35 @@ parseMark :: Parser Instruction
 parseMark = do
     string "Mark"
     char ' '
-    marker <- parseInt
+    marker <- parseMarker
     char ' '
-    st <- parseInt
-    return $ Mark (mkMarker marker) (mkState st)
+    st <- parseState
+    return $ Mark marker st
 
 parseUnmark :: Parser Instruction
 parseUnmark = do
     string "Unmark"
     char ' '
-    marker <- parseInt
+    marker <- parseMarker
     char ' '
-    st <- parseInt
-    return $ Unmark (mkMarker marker) (mkState st)
+    st <- parseState
+    return $ Unmark marker st
 
 parsePickUp :: Parser Instruction
 parsePickUp = do
     string "PickUp"
     char ' '
-    st1 <- parseInt
+    st1 <- parseState
     char ' '
-    st2 <- parseInt
-    return $ PickUp (mkState st1) (mkState st2)
+    st2 <- parseState
+    return $ PickUp st1 st2
 
 parseDrop :: Parser Instruction
 parseDrop = do
     string "Drop"
     char ' '
-    st <- parseInt
-    return $ Drop (mkState st)
+    st <- parseState
+    return $ Drop st
 
 parseTurn :: Parser Instruction
 parseTurn = do
@@ -62,17 +62,17 @@ parseTurn = do
     char ' '
     turn <- string "Left" <|> string "Right"
     char ' '
-    st <- parseInt
-    return $ Turn (if turn !! 0 == 'L' then G.Left else G.Right) (mkState st)
+    st <- parseState
+    return $ Turn (if turn !! 0 == 'L' then G.Left else G.Right) st
 
 parseMove :: Parser Instruction
 parseMove = do
     string "Move"
     char ' '
-    st1 <- parseInt
+    st1 <- parseState
     char ' '
-    st2 <- parseInt
-    return $ Move (mkState st1) (mkState st2)
+    st2 <- parseState
+    return $ Move st1 st2
 
 parseFlip :: Parser Instruction
 parseFlip = do
@@ -80,10 +80,10 @@ parseFlip = do
     char ' '
     n <- parseInt
     char ' '
-    st1 <- parseInt
+    st1 <- parseState
     char ' '
-    st2 <- parseInt
-    return $ Flip n (mkState st1) (mkState st2)
+    st2 <- parseState
+    return $ Flip n st1 st2
 
 parseInt :: Parser Int
 parseInt = do
@@ -113,8 +113,8 @@ parseCondition = do
     case cond of
       "Marker" -> do
           char ' '
-          i <- parseInt
-          return $ Marker (mkMarker i)
+          i <- parseMarker
+          return $ Marker i
       x        -> return (read x :: Condition)
 
 parseSense :: Parser Instruction
@@ -123,12 +123,22 @@ parseSense = do
     char ' '
     dir <- parseSenseDir
     char ' '
-    st1 <- parseInt
+    st1 <- parseState
     char ' '
-    st2 <- parseInt
+    st2 <- parseState
     char ' '
     cond <- parseCondition
-    return $ Sense dir (mkState st1) (mkState st2) cond
+    return $ Sense dir st1 st2 cond
+
+parseState :: Parser Biology.State
+parseState = do
+    i <- parseInt
+    return $ mkState i
+
+parseMarker :: Parser Marker
+parseMarker = do
+    i <- parseInt
+    return $ mkMarker i
 
 readBrainState s = case parse parseInstruction "Brain State" s of
                      Prelude.Left  err -> "ERROR parsing: " ++ show err
