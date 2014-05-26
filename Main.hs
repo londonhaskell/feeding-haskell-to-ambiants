47db5b77f588db7f1c1b6a405b9a3e9e44880d81
Stuart Gale <bishboria@gmail.com>
2014-05-26 15:08:05 +0100

    Filenames & game iterations from command line args

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import System.Environment
+
 import Ants
 import Visualize
 import Geography
@@ -7,10 +9,12 @@ import NeuroCartography
 
 main :: IO ()
 main = do
-    s  <- readFile "world2.txt"
-    rb <- readFile "redBrain.txt"
-    bb <- readFile "blackBrain.txt"
-    let rs = readBrainState $ lines rb
+    args <- getArgs
+    s  <- readFile $ args !! 0  -- world file
+    rb <- readFile $ args !! 1  -- red brain
+    bb <- readFile $ args !! 2  -- black brain
+    let i  = read (args !! 3) :: Int
+        rs = readBrainState $ lines rb
         bs = readBrainState $ lines bb
         w    = mkWorld rs bs 12345
     putStrLn $ render $ parse w s
