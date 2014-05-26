e514437621442a742549c48a76b8ade1844bbf8f
Stuart Gale <bishboria@gmail.com>
2014-05-26 15:09:49 +0100

    Run game by unfolding the result of multistep.

--- Main.hs
+++ Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
+import Data.Map (keys)
+import Data.List (unfoldr)
 import System.Environment
 
 import Ants
 import Visualize
 import Geography
 import NeuroCartography
+import Kinetics
 
 main :: IO ()
 main = do
@@ -16,5 +19,11 @@ main = do
     let i  = read (args !! 3) :: Int
         rs = readBrainState $ lines rb
         bs = readBrainState $ lines bb
-        w    = mkWorld rs bs 12345
-    putStrLn $ render $ parse w s
+        w  = parse (mkWorld rs bs 12345) s
+    mapM_ printInfo $ take i $ go w
+
+printInfo w = do
+    putStrLn $ render w
+    putStrLn $ show . keys $ ants w
+
+go w = unfoldr (\x -> Just (x, multistep x)) w
